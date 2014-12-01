b27f3ccb2faf21b3e24a91bfdc2cee8503e81e54
Daniel Espino Timón <akathorn@gmail.com>
2014-12-01 20:05:30 +0100

    download z3 instead of cloning the repo. Fixes some HTTPS trouble with cloning from codeplex

--- install-z3.sh
+++ install-z3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Download and compile z3
-git clone https://git01.codeplex.com/z3
+
+# v4.3.2
+wget -O z3.zip "http://download-codeplex.sec.s-msft.com/Download/SourceControlFileDownload.ashx?ProjectName=z3&changeSetId=cee7dd39444c9060186df79c2a2c7f8845de415b"
+unzip z3.zip -d z3
+# git clone https://git01.codeplex.com/z3
+
 cd z3
 git checkout rc
 python scripts/mk_make.py
